7cc7671f87e64f9d4701c6da45ee200744e80044
turanegaku <turanegaku@gmail.com>
2015-12-30 20:30:57 +0900

    to error output

--- run.sh
+++ run.sh
@@ -3,15 +3,15 @@ name=`find . -type f -name *.cpp | xargs ls -t | head -n 1`
 md5=`md5 -q $name`
 if [[ -e .prebuild ]]; then
   pmd5=`cat .prebuild`
-  echo "$md5\n$pmd5"
+  echo "$md5\n$pmd5" 1>&2
 fi
 
 if [[ $pmd5 != $md5 ]]; then
   g++ -std=c++11 $name
-  echo "compile"
+  echo "compile" 1>&2
   echo $md5 > .prebuild
 fi
 if [ $? -eq 0 ];then
-  echo "run $name"
+  echo "run $name" 1>&2
   ./a.out
 fi
