c01ec95ccd60a20736298c08d3ff853d8f3f8077
turanegaku <turanegaku@gmail.com>
2016-06-02 21:36:41 +0900

    fix on fail

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+ls *.cpp > /dev/null 2>&1 || (echo 'no cpp file'; exit)
 name=`ls -t *.cpp | head -n 1`
 md5=`md5 -q $name`
-if [[ -e .prebuild ]]; then
+if [ -e .prebuild ]; then
   pmd5=`cat .prebuild`
 fi
 
 if [[ $pmd5 != $md5 ]]; then
-  g++ -std=c++11 $name
+  echo build
+  g++ -std=c++11 $name || exit
   echo $md5 > .prebuild
 fi
-if [ $? -eq 0 ];then
-  echo "run $name"
-  ./a.out
-fi
+echo run $name
+./a.out
