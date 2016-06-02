b52d9117224c2fa2d31f4a18ab3fb2fcac1ce61c
turanegaku <turanegaku@gmail.com>
2016-06-02 20:41:27 +0900

    run newest c++ program

--- run.sh
+++ run.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
-name=`find . -type f -name *.cpp | xargs ls -t | head -n 1`
+name=`ls -t *.cpp | head -n 1`
 md5=`md5 -q $name`
 if [[ -e .prebuild ]]; then
   pmd5=`cat .prebuild`
-  if [[ $1 == '-m' ]]; then
-    echo "$md5\n$pmd5" 1>&2
-  fi
 fi
 
 if [[ $pmd5 != $md5 ]]; then
-  echo "compile" 1>&2
   g++ -std=c++11 $name
-  if [[ $? != 0 ]];then
-    exit 0
-  fi
   echo $md5 > .prebuild
 fi
-
-echo "run $name" 1>&2
-./a.out
+if [ $? -eq 0 ];then
+  echo "run $name"
+  ./a.out
+fi
