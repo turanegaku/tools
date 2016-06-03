587cf14b8b734739248f479d537fb1c107d1f3fa
turanegaku <turanegaku@gmail.com>
2016-06-03 16:35:49 +0900

    -g mode

--- run.sh
+++ run.sh
@@ -8,13 +8,20 @@ ls *.cpp > /dev/null || exit 1
 name=`ls -t *.cpp | head -n 1`
 md5=`md5 -q $name`
 
+# if -g build only
+if [[ $* =~ '-g' ]]; then
+  echo gbuild -> g.out
+  g++ -std=c++11 -g $name
+  exit 0
+fi
+
 # check prebuild file
 prebuild="`dirname $0`/.prebuild"
 [ -e $prebuild ] && pmd5=`cat $prebuild`
 
 # build if different prebuild or -f
 if [[ $pmd5 != $md5 || $* =~ '-f' ]]; then
-  echo build
+  echo build a.out
   g++ -std=c++11 $name || exit 1
   echo $md5 > $prebuild
 fi
